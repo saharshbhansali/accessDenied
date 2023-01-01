4a1e151624fbea41c834d310d0cf7da6e90e09ab
saharshbhansali <saharsh.bhansali@gmail.com>
2023-01-01 23:05:13 +0530

    update: fixes for code failed again
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- Model/client.py
+++ Model/client.py
@@ -24,8 +24,8 @@ async def send_message(server_id, message):
 def run_clients():
     # Send a message to each of the three servers
     a1 = asyncio.run(send_message(0, 'Hello, Server 0!'))
-    a2 = asyncio.run(send_message(1, 'Hello, Server 1!'))
-    a3 = asyncio.run(send_message(2, 'Hello, Server 2!'))
+    a2 = asyncio.run(send_message(0, 'Hello, Server 1!'))
+    a3 = asyncio.run(send_message(0, 'Hello, Server 2!'))
     asyncio.run(asyncio.gather(a1, a2, a3))
     # await asyncio.gather(send_message(0, 'Hello, Server 0!'), send_message(1, 'Hello, Server 1!'), send_message(2, 'Hello, Server 2!'))
 
