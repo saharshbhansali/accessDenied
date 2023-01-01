45b2bde819ec00917e43723d56296ac2290abb58
saharshbhansali <saharsh.bhansali@gmail.com>
2023-01-01 22:32:24 +0530

    update: commented code
    Signed-off-by: saharshbhansali <saharsh.bhansali@gmail.com>

--- Model/client.py
+++ Model/client.py
@@ -1,16 +1,23 @@
 import asyncio
 
+# This is the main function of the client
 async def main():
+    # Connect to the server
     reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
 
+    # Send a message to the server
     message = 'Hello, World!'
     print(f'Send: {message!r}')
     writer.write(message.encode())
     await writer.drain()
 
+    # Wait for a response from the server
     data = await reader.read(100)
+
+    # Print the response from the server
     print(f'Received: {data.decode()!r}')
 
+    # Close the connection
     print('Close the connection')
     writer.close()
 
--- Model/server.py
+++ Model/server.py
@@ -1,17 +1,40 @@
 import asyncio
 
-async def main():
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+# This function is called whenever a new client connects to the server
+async def handle_echo(reader, writer):
+    # Read data from the client
+    data = await reader.read(100)
 
-    message = 'Hello, World!'
-    print(f'Send: {message!r}')
-    writer.write(message.encode())
+    # Decode the data received from the client
+    message = data.decode()
+
+    # Get the address of the client
+    addr = writer.get_extra_info('peername')
+
+    # Print a message indicating that we received data from the client
+    print(f"Received {message!r} from {addr!r}")
+
+    # Send the data back to the client
+    print(f"Send: {message!r}")
+    writer.write(data)
     await writer.drain()
 
-    data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
-
-    print('Close the connection')
+    # Close the connection to the client
+    print("Close the client socket")
     writer.close()
+
+# This is the main function of the server
+async def main():
+    # Create a server with the `handle_echo` function as the handler
+    # for incoming connections
+    server = await asyncio.start_server(handle_echo, '127.0.0.1', 8888)
+
+    # Get the address of the server
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    # Run the server indefinitely
+    async with server:
+        await server.serve_forever()
 
 await main()
